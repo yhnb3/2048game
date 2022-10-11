6101aab91096fc25da1c36549fb018a93a5a0870
yhnb3 <60080270+yhnb3@users.noreply.github.com>
2022-10-11 22:10:01 +0900

    Feat: add 2 or 4 logic

--- src/libs/addNewCell.ts
+++ src/libs/addNewCell.ts
@@ -10,18 +10,28 @@ interface IProps {
  * @returns 새로운 셀이 추가된 함수
  */
 export const addNewCell = ({ matrix }: IProps) => {
+  let possibleFour = false
   const newMatrix = new Array(4).fill(0).map(() =>
     new Array(4).fill(0).map(() => {
       return { prev: 0, current: 0, move: 0, isNew: false, direction: 'Y' }
     })
   )
-  const empty = matrix.reduce((acc, cur) => acc + cur.filter((cell) => cell.current === 0).length, 0)
+  const empty = matrix.reduce(
+    (acc, cur) =>
+      acc +
+      cur.filter((cell) => {
+        if (cell.current >= 16) possibleFour = true
+        return cell.current === 0
+      }).length,
+    0
+  )
   let target = Math.floor(Math.random() * empty)
+  const isFour = possibleFour && Math.random() < 0.2
   for (let i = 0; i < 4; i += 1) {
     for (let j = 0; j < 4; j += 1) {
       if (matrix[i][j].current === 0) {
         if (target === 0) {
-          newMatrix[i][j].current = 2
+          newMatrix[i][j].current = isFour ? 4 : 2
           newMatrix[i][j].isNew = true
         }
         target -= 1
